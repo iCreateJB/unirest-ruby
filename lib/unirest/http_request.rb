c4a6be2ee4e52c79f040dbd0d65700038265dd97
Jonathan Ballard <jonathan.ballard@imagineeasy.com>
2016-10-07 14:39:15 -0400

    Update from master.

--- lib/unirest/http_request.rb
+++ lib/unirest/http_request.rb
@@ -9,10 +9,10 @@
 # distribute, sublicense, and/or sell copies of the Software, and to
 # permit persons to whom the Software is furnished to do so, subject to
 # the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be
 # included in all copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 # EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 # MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
@@ -20,27 +20,27 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-# 
+#
 
 require 'addressable/uri'
 require "base64"
 
 module Unirest
-  
+
   class HttpRequest
     attr_reader :method
-    attr_reader :url 
+    attr_reader :url
     attr_reader :headers
     attr_reader :body
     attr_reader :auth
-    
+
     def add_header(name, value)
       @headers[name] = value
     end
-    
+
     def initialize(method, url, headers = {}, body = nil, auth = nil)
       @method = method
-      
+
       if method == :get
         if body.is_a?(Hash) && body.length > 0
           if url.include? "?"
@@ -60,9 +60,9 @@ module Unirest
       unless url =~ URI.regexp
         raise "Invalid URL: #{url}"
       end
-      
+
       @url = url.gsub /\s+/, '%20'
-      
+
       @headers = {}
 
       if auth != nil && auth.is_a?(Hash)
@@ -79,11 +79,10 @@ module Unirest
 
       end
 
-
       # Make the header key lowercase
       headers.each_pair {|key, value| @headers[key.downcase] = value }
     end
-    
+
   end
-  
+
 end
